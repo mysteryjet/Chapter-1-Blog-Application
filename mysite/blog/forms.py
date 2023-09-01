b681d5d544de0c564afcd26b949f099bcbcec89b
Manuel Gualito <elmanubc@gmail.com>
2023-09-01 00:02:14 -0600

    Se completó el form de comments desde models

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
@@ -6,4 +7,12 @@ class EmailPostForm(forms.Form):
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
 
-    
+
+# to create a form from a model, we indicate which model to build the form for in the meta class
+# of the form
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
+
+
