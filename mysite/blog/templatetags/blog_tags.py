78a83e9f33f1ba83159b654051db3f8c4e3995ae
Manuel Gualito <elmanubc@gmail.com>
2023-09-02 02:45:00 -0600

    Se completó la creación de una inclusion tag, renderiza un template con context variables

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -11,3 +11,11 @@ register = template.Library()
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+# specified the template that will be rendered with the returned values using blog/
+# post/latest_posts.html
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
